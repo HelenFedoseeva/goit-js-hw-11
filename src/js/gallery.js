e22ef2084a0205c81ec274f9571b6c00107b3315
Helen <helenlena52@gmail.com>
2022-10-28 21:51:48 +0300

    delete-scroll-on-submit

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -62,15 +62,6 @@ async function onSubmitHandler(e) {
     Notiflix.Notify.info(`Hooray! We found ${receivedData.totalHits} images`);
     renderMarkup(receivedData);
 
-    const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
-
     let lightBox = new SimpleLightbox('.gallery a', {
       enableKeyboard: true,
       docClose: true,
